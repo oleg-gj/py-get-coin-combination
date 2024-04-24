14a010fd1f45d7db794adb0729ee9a1d4582a8ec
Oleh <musiychuk09@gmail.com>
2024-04-24 17:32:32 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,31 @@
+import pytest
+
+from typing import Any
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "coins,result",
+    [
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (50, [0, 0, 0, 2]),
+    ]
+)
+def test_coin(coins: int, result: list[int]) -> None:
+    assert get_coin_combination(coins) == result
+
+
+@pytest.mark.parametrize(
+    "coins",
+    [
+        "3",
+        {1: 5},
+        (3, 5, 8),
+    ]
+)
+def test_coin_type(coins: Any) -> None:
+    with pytest.raises(TypeError):
+        get_coin_combination(coins), "Incorrect data type provided"
